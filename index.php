504c7270f88cc80110b8753c1be2d32dd9873479
Aldis Elfarsdottir <aelfarsdottir@college.harvard.edu>
2015-11-26 22:01:00 +0000

    somehow needed to recreate helpers.php file?? when I pulled from the master branch (which had it), it disappeared from ide

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   // {
   //   // pass in variable background images?
   //   // render homepage
-  render("home.php", ["title" => "Hello, Haiku"]);
+  render("/views/home.php", ["title" => "Hello, Haiku"]);
   // }
   
 ?>
