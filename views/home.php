cba0e5bf60917314d8c2bb32d7dcd2fea8e4d913
Aldis Elfarsdottir <aelfarsdottir@college.harvard.edu>
2015-11-26 20:32:30 +0000

    modeling home.php after portfolio.php

--- views/home.php
+++ views/home.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    Hello, Haiku!
-    <p></p><img src="http://img11.deviantart.net/8f9c/i/2009/101/d/3/fuji_cherry_blossoms__by_zeroai.jpg" alt="Cherry Blossoms" height="42" width="42"></p>
-  </body>
-  <form>
-  <input type="button" value="Generate!" onclick="window.location.href='http://aelfarsdottir.github.io/haiku.php'" />
-  </form>
-</html>
+<div>
+
+    <body>
+        Hello, Haiku!
+    <p></p><img src="http://img11.deviantart.net/8f9c/i/2009/101/d/3/fuji_cherry_blossoms__by_zeroai.jpg" alt="Cherry Blossoms" height="142" width="142"></p>
+    </body>
+    
+    <form>
+        <input type="button" value="Generate!" onclick="window.location.href='http://aelfarsdottir.github.io/haiku.php'" />
+    </form>
+    
+<div>
