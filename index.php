798dcf567e7f98b109f8a68e1c7781b1c17144a7
Aldis Elfarsdottir <aelfarsdottir@college.harvard.edu>
2015-11-26 22:33:02 +0000

    undoing that which didn't work. next step: going to try to just write index.php as home.html and include the header and footer in the home.html, as well as in haiku.html

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-<div>
-    <body>
-        Hello, Haiku!
-    <p></p><img src="http://img11.deviantart.net/8f9c/i/2009/101/d/3/fuji_cherry_blossoms__by_zeroai.jpg" alt="Cherry Blossoms" height="142" width="142"></p>
-    </body>
-    
-    <!--we want to submit the generate button click via POST in order to call draft.php and generate a haiku-->
-    <form action="draft.php" method="POST">
-        <fieldset>
-            <div class="form-group">
-                <button class="btn btn-default" type="submit">
-                    <span aria-hidden="true"></span>
-                    Generate!
-                </button>
-            </div>
-        </fieldset> 
-    </form>
-<div>
-
-<!--<? php-->
+<? php
   
   // configuration
-<!--  require("helpers.php");-->
+  require("helpers.php");
   
   // necessary? GET vs. POST. won't we only be doing this via GET? 
   // clicking the button "Generate!" sends a GET request to the draft.php, which sends the generated lines as it renders the haiku.php page
@@ -31,9 +10,7 @@
   // {
   //   // pass in variable background images?
   //   // render homepage
-<!--  render("home.html", ["title" => "Hello, Haiku"]);-->
+  render("home.html", ["title" => "Hello, Haiku"]);
   // }
   
-<!--?>-->
-
-</html>
+?>
